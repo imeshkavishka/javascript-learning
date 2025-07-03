5900d73079ceefcfef0e0614ec3ca1b220f30f16
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-03 11:47:09 +0530

    Assignment Operation Done

---
+++ Assignment Operators/ao.js
@@ -0,0 +1,19 @@
+var age = 23;
+
+age+=10;
+console.log(age);
+
+age-=4;
+console.log(age);
+
+age*=2;
+console.log(age);
+
+age/=2;
+console.log(age);
+
+age%=3;
+console.log(age);   
+
+age**=2;
+console.log(age);
