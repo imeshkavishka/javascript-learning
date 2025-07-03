6981a13f1087439c6ff1528d29bfe1fc7f4660b5
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-03 11:45:35 +0530

    Comparison Operation Done

---
+++ Comparison Operators/co.js
@@ -0,0 +1,19 @@
+var a=10;
+var b='10';
+var c=35;
+var d=5;
+
+if (a==b){
+    if (a===b){
+        console.log('a and b are equal in value and type');
+ 
+    }
+    else if (a !== b){ 
+        console.log('a and b are equal in value or type');
+
+    }
+}
+else{
+    console.log('a and b are not equal');
+}
+c>=5 ? console.log('c is greater than or equal to 5') : console.log('c is less than 5');
