7a32bb3968714f23c33374fc26e33d615009d865
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-03 15:27:23 +0530

    Array Done

---
+++ Array/array.js
@@ -0,0 +1,8 @@
+var array =[ruwan,Nimal,Amara,Piyal]
+var marks = new array(5,10,15,20,25);
+
+console.log(array);
+console.log(marks.length);
+
+console.log(array[0]+"Have a "+marks[0]+" marks for the exam");
+
