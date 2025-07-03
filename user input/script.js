db21f351ae35f8b72d0690626f4282c23d7c9e5c
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-03 11:37:05 +0530

    user input done

---
+++ user input/script.js
@@ -0,0 +1,27 @@
+console.log("Good Day!");
+
+var firstName = "Imesh";
+var lastName = "Kavishka";
+var age = 23;
+var isok = true;
+var hegiht,weight;
+
+console.log(firstName);
+console.log('my Last Name Is '+lastName);
+console.log('My Age' + age);
+console.log('Are You Ok? ' + isok);
+console.log(hegiht);
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isok);
+console.log(typeof hegiht);
+
+lastName = "koongoda";
+hegiht = 5.8;
+
+console.log('My Name is' +lastName +'My Height is ' + hegiht);
+
+alert('My Name is'+firstName+' '+lastName+'.My Age is '+age+'. Are you ok?');
+weight = prompt('Enter your weight');
+alert('My Weight is ' + weight);
