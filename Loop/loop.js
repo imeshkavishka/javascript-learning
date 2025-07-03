a69759b8788a85ef65b8fadae38c3a3939bf536b
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-03 23:12:14 +0530

    Loop Done

---
+++ Loop/loop.js
@@ -0,0 +1,30 @@
+for(var i=0; i<10; i++){
+    console.log(i);
+}
+
+var students=['imesh','kavishka','nimesh','sahan','sajith'];
+for (var i=0; i<students.length; i++){
+    console.log(students[i]);
+}
+
+
+var fulname="imesh kavishka";
+var j;
+for (j of fulname){
+    console.log(j);
+}
+
+var marks=[10,20,30,40,50];
+var k=0;
+while(k<marks.length){
+    console.log(marks[k]);
+    k++;
+}
+
+var l=0;
+do{
+    console.log(marks[l]);
+    l--;
+}while(l>=1);    
+
+
