4854a27d5cc1ff8e550364ca85a9a577671bb8e1
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-03 11:43:23 +0530

    Arithmatic Operations Done

---
+++ Arithmatic Operators/operators.js
@@ -0,0 +1,35 @@
+
+
+var myage,yourtage,value;
+myage = 25;
+yourage = 30;
+
+myage= myage + 5; 
+console.log('after the 5 yers in my age '+myage);
+
+yourage = yourage - 14; 
+console.log('before the 14 yers in my age '+yourage); 
+
+value = myage * yourage; 
+console.log('the value of my age and your age is '+value); 
+
+value =myage / yourage;
+console.log('the value of my age and your age is '+value);
+
+value = myage % yourage;
+console.log('the value of my age and your age is '+value);     
+
+console.log(myage**2);
+
+myage=(myage++);
+console.log('post incriment of my age '+myage);
+
+myage=(++myage);
+console.log('pre incriment of my age '+myage);
+
+var cal=(45/5)*4/5+80-41;
+console.log('the value of cal is '+cal);
+
+var num1,num2;
+nam1 = num2 = (52*5)/2+4-5;
+console.log( num1+'|'+num2 );
