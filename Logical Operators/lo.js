4cde2e7bc8e7eb1cff6cc72f4a6e9f050ba674eb
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-03 11:51:57 +0530

    Logical Operators Done

---
+++ Logical Operators/lo.js
@@ -0,0 +1,20 @@
+var num1 =56;
+var num2 = 45;
+var num3= 78;
+
+if (num1 >=100 && num1 <= 75){
+    console.log('Grade A');
+
+}
+else if(num1>75 && num1 <=50){
+    console.log('Grade B');
+}
+else if(num1>50 || num2 >50){
+    console.log('Pass');
+}
+else if(num1 != num3){
+    console.log('Not equal');
+}
+else{
+    console.log('Done');
+}
