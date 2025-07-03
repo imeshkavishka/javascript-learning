7463ac1ed4082bf1d1532ed8e151f042b6a2587b
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-03 11:48:59 +0530

    Bitwise Operatorer Done

---
+++ Bitwise Operators/bo.js
@@ -0,0 +1,9 @@
+console.log(~5);
+
+console.log(5^1);
+console.log(5<<1);
+console.log(5>>1);
+console.log(5>>>1);
+console.log((10).toString(2));
+console.log((0b1010).toString(10));
+console.log(parseInt ('1010', 2));
