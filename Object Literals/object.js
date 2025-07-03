83de784a2d3f4208282bb24ca6772a3793f3529b
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-03 16:57:37 +0530

    Object Literals

---
+++ Object Literals/object.js
@@ -0,0 +1,15 @@
+var person = {
+    firstNAme: "John",
+    lastName: "Doe",
+    age:30,
+    subjects :['JavaScript', 'Python', 'Java']
+
+}
+
+console.log(person);
+console.log(person.subjects);
+console.log(person.subjects[2]);
+var x = 'age';
+console.log(person[x]);
+
+console.log(person['subjects'[0]]);
