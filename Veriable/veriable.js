98668cb8ce78ff0802e759d5cacb993e741f67fb
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-04 10:43:15 +0530

    Veriable Done

---
+++ Veriable/veriable.js
@@ -0,0 +1,8 @@
+var subject ="Maths";
+function fsub(){
+    var subject = "Science";
+    console.log("Inside function, subject is: " + subject);
+}
+
+console.log("Outside function, subject is: " + subject);
+(fsub);
